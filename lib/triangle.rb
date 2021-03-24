05184ec6837c7e1a40b72f727e299746f55e2ea9
Clayon Stewart <c.stewart89@gmail.com>
2021-03-24 13:15:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  
+  @@@all =[]
+  
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c 
+    self.class.all << self
+  end
+  
+  def kind
+    if( a + b ) < c
+      raise TriangleError
+      rescue TriangleError => 
+        message "sides do not match"
+    end
+  end  
+  
+  
+  class TriangleError < StandardError
+    
+  end
 end
